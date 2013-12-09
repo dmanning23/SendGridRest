19c94875e2764e758558d54c2b888a436870b2d4
Daniel Manning <daniel.manning@demandmedia.com>
2013-12-09 14:47:58 -0600

    got unsubscribe stuff working

--- Resubscribe/Program.cs
+++ Resubscribe/Program.cs
@@ -19,23 +19,19 @@ namespace Resubscribe
 			//First item is the sendgrid username
 			string username = args[0];
 
-			//second item it the sendgrid password
+			//second item is the sendgrid password
 			string pwd = args[1];
 
-			//third item it the sendgrid password
+			//third item is the email to resubscribe
 			string email = args[2];
 
-			/*
-			 * POST 	https://api.sendgrid.com/api/unsubscribes.delete.json
-POST Data 	api_user=your_sendgrid_username&api_key=your_sendgrid_password&email=emailToDelete@domain.com
-			 * */
-
+			//hit the sendgrid rest endpoint
 			var client = new RestClient();
 			client.BaseUrl = "https://api.sendgrid.com";
 			var request = new RestRequest("api/unsubscribes.delete.json");
 			request.AddParameter("api_user", username); // used on every request
 			request.AddParameter("api_key", pwd); // used on every request
-			request.AddParameter("email", email); // used on every request
+			request.AddParameter("email", email);
 
 			IRestResponse response = client.Execute(request);
 			string responseText = response.Content;
--- Unsubscribe/Program.cs
+++ Unsubscribe/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using RestSharp;
 
 namespace Unsubscribe
 {
@@ -10,6 +12,45 @@ namespace Unsubscribe
 	{
 		static void Main(string[] args)
 		{
+			//Get the items out of the command line
+			if (args.Length < 2)
+			{
+				Console.WriteLine("usage: Unsubscribe <username> <password> <email>");
+				return;
+			}
+
+			//First item is the sendgrid username
+			string username = args[0];
+
+			//second item is the sendgrid password
+			string pwd = args[1];
+
+			//third item is the email to unsubscribe
+			string email = args[2];
+
+			//hit the sendgrid rest endpoint
+			var client = new RestClient();
+			client.BaseUrl = "https://api.sendgrid.com";
+			var request = new RestRequest("api/unsubscribes.add.json");
+			request.AddParameter("api_user", username); // used on every request
+			request.AddParameter("api_key", pwd); // used on every request
+			request.AddParameter("email", email);
+
+			IRestResponse response = client.Execute(request);
+			string responseText = response.Content;
+
+			var thing = JsonConvert.DeserializeObject<UnsubscribeResponse>(response.Content);
+
+			Console.WriteLine(responseText);
+		}
+
+		public class UnsubscribeResponse
+		{
+			[JsonProperty("message")]
+			public string Message { get; set; }
+
+			[JsonProperty("errors")]
+			public List<string> Errors { get; set; }
 		}
 	}
 }
